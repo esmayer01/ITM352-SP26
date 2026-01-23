54d367415147e1da88febe7c1d0e2da134eb8f6c
ESMAYER <esmayer@hawaii.edu>
2026-01-22 20:15:13 -1000

    Update function_example_.py

--- Lab 3/function_example_.py
+++ Lab 3/function_example_.py
@@ -1,12 +1,16 @@
 # An example of creating and user your own function
 # Name: Ethan Mayer
 # Date: Jan. 22, 2026
+import datetime
 
 def greet(name):
     """This function greets the person whose name is passed in.
     In addition we want to print a welcome message that includes
     the day of the week"""
     message = "Hello" + " " + name + "!"
+    x = datetime.datetime.now()
+    day_of_week = x.strftime("%A")
+    message += " Happy " + day_of_week + "!"
     return message
 
 user_name = input("Please enter your name: ")
